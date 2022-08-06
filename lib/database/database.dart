b8949c600815a3cb6588f024bd24d0c64446100c
AUNG533 <aungkyawmon@gmail.com>
2022-08-06 13:03:52 +0700

    getNoteMapList, getNoteList, insertNote, updateNote, deleteNote

--- lib/database/database.dart
+++ lib/database/database.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
+import 'package:sql_note_check_box/models/note_model.dart';
 
 class DatabaseHelper {
   static final DatabaseHelper _instance = DatabaseHelper.internal();
@@ -52,5 +53,58 @@ class DatabaseHelper {
         $colStatus INTEGER
       )
     """);
+  }
+
+  // get all notes from database
+  Future<List<Map<String, dynamic>>> getNoteMapList() async {
+    Database? db = await this.db;
+    final List<Map<String, dynamic>> result = await db!.query(noteTable);
+    return result;
+  }
+
+  // get note list from database
+  Future<List<Note>> getNoteList() async {
+    final List<Map<String, dynamic>> noteMapList = await getNoteMapList();
+
+    final List<Note> noteList = [];
+
+    for (var noteMap in noteMapList) {
+      noteList.add(Note.fromMap(noteMap));
+    }
+    noteList.sort((noteA, noteB) => noteA.date!.compareTo(noteB.date!));
+    return noteList;
+  }
+
+  // insert note to database
+  Future<int> insertNote(Note note) async {
+    Database? db = await this.db;
+    final int result = await db!.insert(
+      noteTable,
+      note.toMap(),
+    );
+    return result;
+  }
+
+  // update note in database
+  Future<int> updateNote(Note note) async {
+    Database? db = await this.db;
+    final int result = await db!.update(
+      noteTable,
+      note.toMap(),
+      where: '$colId = ?',
+      whereArgs: [note.id],
+    );
+    return result;
+  }
+
+  // delete note from database
+  Future<int> deleteNote(int id) async {
+    Database? db = await this.db;
+    final int result = await db!.delete(
+      noteTable,
+      where: '$colId = ?',
+      whereArgs: [id],
+    );
+    return result;
   }
 }
